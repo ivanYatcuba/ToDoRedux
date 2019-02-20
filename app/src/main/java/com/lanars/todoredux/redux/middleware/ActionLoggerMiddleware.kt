428ca0b367cc9e43ec49945ec48d1a3bd7ab0be9
ivan <ivan@lanars.com>
2019-02-20 12:39:18 +0200

    Some kotlin style string formatting

--- app/src/main/java/com/lanars/todoredux/redux/middleware/ActionLoggerMiddleware.kt
+++ app/src/main/java/com/lanars/todoredux/redux/middleware/ActionLoggerMiddleware.kt
@@ -8,7 +8,7 @@ import com.lanars.todoredux.redux.store.ReduxStore
 class ActionLoggerMiddleware : ReduxMiddleware<AppState, AppAction, ReduxStore<AppState, AppAction>>() {
     override fun doBeforeDispatch(store: ReduxStore<AppState, AppAction>, action: AppAction) {
         Log.d("====ACTION===", "=============================")
-        Log.d("====ACTION===", action.javaClass.canonicalName + ": " + action.toString())
+        Log.d("====ACTION===", "${action.javaClass.canonicalName}:$action")
         Log.d("====ACTION===", "=============================")
     }
 
